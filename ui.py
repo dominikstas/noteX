4c0ae3fe17555d8dafb9b81771bc1c5eb07aa578
Dominik Stasiak <d.stasiak436@gmail.com>
2023-10-11 20:37:13 +0200

    minor code fixes

--- ui.py
+++ ui.py
@@ -1,45 +1,41 @@
 import sys
-import typing
-from database import Database 
-from PyQt5.QtWidgets import  QShortcut, QMenuBar, QCheckBox, QAction, QApplication, QToolBar, QMessageBox,  QMainWindow, QVBoxLayout, QTextEdit, QPushButton, QWidget, QFileDialog
+from PyQt5.QtWidgets import (
+    QShortcut, QMenuBar, QCheckBox, QAction, QApplication,
+    QToolBar, QMessageBox, QMainWindow, QVBoxLayout,
+    QTextEdit, QPushButton, QWidget, QFileDialog
+)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeySequence, QTextCursor, QTextListFormat
+from database import Database
 
 class UiClass(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        #current file
+        # Current file
         self.current_file = None
         self.db = Database('settings.db')
-
         self.initUI()
-
         self.load_dark_mode_setting()
 
-        #font
+        # Font
         self.font_size = 12
-        
-        #h1
+
+        # Shortcuts
         increase_font_shortcut = QShortcut(QKeySequence("Ctrl+1"), self)
         increase_font_shortcut.activated.connect(self.increase_font)
 
-        #h1 - back
         decrease_font_shortcut = QShortcut(QKeySequence("Ctrl+2"), self)
         decrease_font_shortcut.activated.connect(self.decrease_font)
 
-
-
-
     def initUI(self):
+        self.dark_mode = False
 
-        self.dark_mode = False 
-
-        #save = ctrl + s
+        # Save shortcut
         save_shortcut = QShortcut(QKeySequence("Ctrl+S"), self)
         save_shortcut.activated.connect(self.save_note)
 
-        #main window
+        # Main window
         self.setGeometry(100, 100, 800, 600)
         self.setWindowTitle('NoteX')
 
@@ -48,65 +44,59 @@ class UiClass(QMainWindow):
 
         layout = QVBoxLayout(central_widget)
 
-        #text
+        # Text
         self.text_edit = QTextEdit(self)
         layout.addWidget(self.text_edit)
 
-
-        #navbar
+        # Navbar
         toolbar = QToolBar(self)
         self.addToolBar(toolbar)
-        
 
-        #save button
+        # Save button
         save_button = QPushButton('Save', self)
         save_button.clicked.connect(self.save_note)
         toolbar.addWidget(save_button)
 
-        #dark mode
+        # Dark mode button
         dark_mode_button = QPushButton('Change mode', self)
         dark_mode_button.clicked.connect(self.toggle_dark_mode)
         toolbar.addWidget(dark_mode_button)
 
-
-        #navbar menu
+        # Navbar menu
         menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu('note')
+        file_menu = menu_bar.addMenu('Note')
 
-
-        #menu - new file
+        # New note action
         new_note_action = QAction('New note', self)
         new_note_action.triggered.connect(self.new_note)
         file_menu.addAction(new_note_action)
 
-
-        #menu - open file
+        # Open note action
         open_note_action = QAction('Open note', self)
         open_note_action.triggered.connect(self.open_note)
         file_menu.addAction(open_note_action)
 
-
-        #menu - save file
+        # Save note action
         save_note_action = QAction('Save', self)
         save_note_action.triggered.connect(self.save_note)
         file_menu.addAction(save_note_action)
 
-        #choose main mode
+        # Choose main mode
         dark_mode_checkbox = QCheckBox('Automate mode', self)
         dark_mode_checkbox.setChecked(self.dark_mode)
         dark_mode_checkbox.stateChanged.connect(self.toggle_auto_dark_mode)
         toolbar.addWidget(dark_mode_checkbox)
 
-        #list button
+        # List button
         bullet_list_button = QPushButton('Bullet list', self)
         bullet_list_button.clicked.connect(self.insert_bullet_list)
         toolbar.addWidget(bullet_list_button)
 
-    #save button function
+    # Save button function
     def save_note(self):
         if self.current_file is None:
             options = QFileDialog.Options()
-            options != QFileDialog.DontUseNativeDialog 
+            options != QFileDialog.DontUseNativeDialog
             file_name, _ = QFileDialog.getSaveFileName(self, "Save file", ".txt", "Text files (*.txt);;All files (*)", options=options)
             if file_name:
                 with open(file_name, 'w') as file:
@@ -117,26 +107,17 @@ class UiClass(QMainWindow):
             with open(self.current_file, 'w') as file:
                 file.write(self.text_edit.toPlainText())
             self.update_window_title()
-    
-    
 
-    ### Alert ### 
-
-    #check document
+    # Alert
     def check_modified_document(self):
         if self.text_edit.document().isModified():
-
-        # Alert
-            reply = QMessageBox.question(self, 'Save changes', 'Do you want to save the note?', 
-                                     QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
-        # Save
+            reply = QMessageBox.question(self, 'Save changes', 'Do you want to save the note?',
+                                         QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
             if reply == QMessageBox.Yes:
                 self.save_note()
-        # Cancel
             elif reply == QMessageBox.Cancel:
                 return False
         return True
-
 
     def closeEvent(self, event):
         if not self.check_modified_document():
@@ -144,13 +125,13 @@ class UiClass(QMainWindow):
         else:
             event.accept()
 
-#new note
+    # New note
     def new_note(self):
         if not self.check_modified_document():
             return
         self.text_edit.clear()
 
-#open note
+    # Open note
     def open_note(self):
         if not self.check_modified_document():
             return
@@ -163,18 +144,15 @@ class UiClass(QMainWindow):
                 self.current_file = file_name
                 self.update_window_title()
 
-#dark mode
+    # Dark mode
     def toggle_dark_mode(self):
         self.dark_mode = not self.dark_mode
         self.update_theme()
 
     def update_theme(self):
-        if self.dark_mode:
-            self.setStyleSheet("background-color: black; color: lightgray;")
-            self.text_edit.setStyleSheet("background-color: black; color: lightgray;")
-        else:
-            self.setStyleSheet("background-color: white; color: black;")
-            self.text_edit.setStyleSheet("background-color: white; color: black;")
+        theme = "background-color: black; color: lightgray;" if self.dark_mode else "background-color: white; color: black;"
+        self.setStyleSheet(theme)
+        self.text_edit.setStyleSheet(theme)
 
     def update_window_title(self):
         if self.current_file:
@@ -182,29 +160,23 @@ class UiClass(QMainWindow):
         else:
             self.setWindowTitle('NoteX')
 
-
     def load_dark_mode_setting(self):
         self.dark_mode = self.db.load_dark_mode_setting()
         self.update_theme()
-
-
 
     def toggle_auto_dark_mode(self, state):
         self.dark_mode = state == Qt.Checked
         self.db.save_dark_mode_setting(self.dark_mode)
         self.update_theme()
-    
 
-    ### Font ###
-
-    #h1 
+    # Font
+    # Increase font size
     def increase_font(self):
         self.update_font(3)
 
-    #h1 - back
+    # Decrease font size
     def decrease_font(self):
         self.update_font(-3)
-
 
     def update_font(self, num):
         cursor = self.text_edit.textCursor()
@@ -216,11 +188,11 @@ class UiClass(QMainWindow):
         char_format.setFontPointSize(new_size)
         cursor.mergeCharFormat(char_format)
 
-    #bullet list
+    # Bullet list
     def insert_bullet_list(self):
         cursor = self.text_edit.textCursor()
 
-        #check if its on or off
+        # Check if it's on or off
         cursor.movePosition(QTextCursor.StartOfBlock)
         list_format = cursor.blockFormat().toListFormat()
 
